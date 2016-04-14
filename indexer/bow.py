4bbdb6e3de303d5b35b293ee0dde58fbcc634f47
Nicolas Dugué <dugue.nic@gmail.com>
2016-04-14 16:05:26 +0200

    Objects to store in the sorted sets

---
+++ indexer/bow.py
@@ -0,0 +1,36 @@
+'''
+Created on Apr 14, 2016
+
+@author: dugue
+'''
+
+class WordFrequency:
+    def __init__(self, word, frequency=0):
+        self.__word=word
+        self.frequency=frequency
+        
+    def incrFrequecy(self):
+        self.frequency+=1
+    
+    #For quick search in set
+    def __hash__(self):
+        return self.__word.__hash__()
+    
+    #For sort in set
+    def __cmp__(self, other):
+        if self.__word == other.__word:
+            return 0
+        else:
+            value= self.frequency - other.frequency
+            if (value == 0):
+                return self.__word.__hash__() - other.__word.__hash__()
+            return value
+    
+    #For quick search in set
+    def __lt__(self, other):
+        return self.__word.__hash__() < other.__word.__hash__()
+    
+    def __str__(self):
+        return "{ "+self.__word+" , "+ str(self.frequency)+"} "
+
+        
---
+++ indexer/testOrderedSet.py
@@ -0,0 +1,67 @@
+'''
+Created on Apr 14, 2016
+
+@author: dugue
+'''
+from sortedcontainers import  SortedSet
+from bow import WordFrequency
+
+
+
+w1=WordFrequency("le",200)
+w2=WordFrequency("et",220)
+w3=WordFrequency("a", 25)
+
+wordSet=SortedSet()
+wordSet.add(w3)
+wordSet.add(w1)
+wordSet.add(w2)
+wordSet.add(w3)
+wordSet.add(w3)
+wordSet.add(w1)
+wordSet.add(w2)
+w3=WordFrequency("a", 21)
+wordSet.add(w3)
+w3=WordFrequency("b", 25)
+wordSet.add(w3)
+w3=WordFrequency("b", 20)
+wordSet.add(w3)
+w3=WordFrequency("ab", 25)
+wordSet.add(w3)
+_set="Set : "
+for i in range(len(wordSet)):
+    _set+= str(wordSet[i])
+print _set+"\n"
+    
+w4=WordFrequency("ab", 1)
+print w4, " in set ? ", w4 in wordSet, wordSet.index(w4)
+w4=WordFrequency("ab", 200000)
+print w4, " in set ? ",w4 in wordSet, wordSet.index(w4)
+w4=WordFrequency("pooooop", 25)
+print w4, " in set ? ", w4 in wordSet
+w4=WordFrequency("le", 0)
+print w4, " in set ? ",w4 in wordSet, "position : ", wordSet.index(w4)
+w4=WordFrequency("le", 25)
+print w4, " in set ? ",w4 in wordSet, "position : ", wordSet.index(w4)
+wordSet[wordSet.index(w4)].incrFrequecy()
+
+
+
+w4=WordFrequency("le", 25)
+wordSet.add(w4)
+wordSet[wordSet.index(w4)].incrFrequecy()
+
+_set="Set : "
+for i in range(len(wordSet)):
+    _set+= str(wordSet[i])
+print _set+"\n"
+
+w4=WordFrequency("O", 0)
+wordSet.add(w4)
+wordSet[wordSet.index(w4)].incrFrequecy()
+
+_set="Set : "
+for i in range(len(wordSet)):
+    _set+= str(wordSet[i])
+print _set+"\n"
+
