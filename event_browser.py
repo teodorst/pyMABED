9ea7431e950e6d0866ea041555a4bcfbbb77f7ab
Adrien Guille <adrien.guille@univ-lyon2.fr>
2016-04-17 19:39:05 +0200

    Improve event browser

--- event_browser.py
+++ event_browser.py
@@ -2,47 +2,78 @@
 from mabed import MABED
 from corpus import Corpus
 from flask import Flask, render_template
-import time, datetime
+import time
+import timeit
+import pickle
 
 __author__ = "Adrien Guille"
 __email__ = "adrien.guille@univ-lyon2.fr"
 
-# load and prepare a tweet corpus
-my_corpus = Corpus('input/messages2.csv')
-my_corpus.discretize(120)
-print 'Stop words:', my_corpus.stop_words
-print 'Corpus: %i tweets, spanning from %s to %s' % (my_corpus.size, my_corpus.start_date, my_corpus.end_date)
-print 'Vocabulary: %i unique tokens' % len(my_corpus.vocabulary)
+print 'Loading corpus...'
+start_time = timeit.default_timer()
+my_corpus = Corpus('input/messages3.csv')
+elapsed = timeit.default_timer() - start_time
+print 'Corpus loaded in %f seconds.' % elapsed
 
-# run MABED
+time_slice_length = 30
+print 'Partitioning tweets into %d-minute time-slices...' % time_slice_length
+start_time = timeit.default_timer()
+my_corpus.discretize(time_slice_length)
+print '   Time-slices: %i' % my_corpus.time_slice_count
+elapsed = timeit.default_timer() - start_time
+print 'Partitioning done in %f seconds.' % elapsed
+
+print 'Running MABED...'
+k = 10
+theta = 0.6
+sigma = 0.6
+start_time = timeit.default_timer()
 mabed = MABED(my_corpus)
-events = mabed.run(k=10, theta=0.6, sigma=0.6)
+mabed.run(k=k, theta=theta, sigma=sigma)
+elapsed = timeit.default_timer() - start_time
+print 'Top %d events detected in %f seconds.' % (k, elapsed)
 
-# instantiate a Flask Web server
-app = Flask(__name__, static_folder='browser/static', template_folder='browser/templates')
 
 # format data
-js_elements = []
+event_descriptions = []
+impact_data = []
 formatted_dates = []
-for i in range(0, my_corpus.time_slice_count):
-    formatted_dates.append(long(time.mktime(my_corpus.to_date(i).timetuple())))
-for event in events:
+for i in range(0, mabed.corpus.time_slice_count):
+    formatted_dates.append(str(mabed.corpus.to_date(i)))
+for event in mabed.events:
+    mag = event[0]
     main_term = event[2]
     raw_anomaly = event[4]
     formatted_anomaly = []
     time_interval = event[1]
-    for i in range(0, my_corpus.time_slice_count):
+    related_terms = []
+    for related_term in event[3]:
+        related_terms.append(related_term[0]+' ('+str("{0:.2f}".format(related_term[1]))+')')
+    event_descriptions.append((mag,
+                               str(mabed.corpus.to_date(time_interval[0])),
+                               str(mabed.corpus.to_date(time_interval[1])),
+                               main_term,
+                               ', '.join(related_terms)))
+    for i in range(0, mabed.corpus.time_slice_count):
         value = 0
         if time_interval[0] <= i <= time_interval[1]:
             value = raw_anomaly[i]
         formatted_anomaly.append('['+str(formatted_dates[i])+','+str(value)+']')
-    js_elements.append('{"key":"'+main_term+'", "values":['+','.join(formatted_anomaly)+']}')
+    impact_data.append('{"key":"' + main_term + '", "values":[' + ','.join(formatted_anomaly) + ']}')
+
+# instantiate a Flask Web server
+app = Flask(__name__, static_folder='browser/static', template_folder='browser/templates')
 
 
 @app.route('/')
 def index():
-    return render_template('event_impact.html', event_impact='['+','.join(js_elements)+']')
+    return render_template('template.html',
+                           events=event_descriptions,
+                           event_impact='['+','.join(impact_data) + ']',
+                           k=k,
+                           theta=theta,
+                           sigma=sigma)
 
 if __name__ == '__main__':
     # Access the browser at http://localhost:2016/
-    app.run(debug=True, host='localhost', port=2016)
+    app.run(debug=False, host='localhost', port=2016)
